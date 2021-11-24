33e3ce42da0879d898f1a431380924fe7d5a52a9
bauleko103 <59721895+bauleko103@users.noreply.github.com>
2021-11-24 08:51:00 +0700

    done

--- ADBTeam2111COMP104102_DB4O/ADBTeam2111COMP104102_DB4O/DB/Database.cs
+++ ADBTeam2111COMP104102_DB4O/ADBTeam2111COMP104102_DB4O/DB/Database.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using Db4objects.Db4o;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ADBTeam2111COMP104102_DB4O.EF;
 
 namespace ADBTeam2111COMP104102_DB4O.DB
 {
     class Database
     {
+        public static string DbFileName { get; set; }
+        public static IObjectContainer DB = null;
+        public static void Open()
+        {
+            DB = Db4oEmbedded.OpenFile(DbFileName);
+        }
+        public static void CloseDB() { DB.Close(); }
+
+        public static void Store<T>(T obj)
+        {
+            try
+            {
+                DB.Store(obj);
+                DB.Commit();
+            }
+            catch
+            {
+                DB.Rollback();
+            }
+        }
+        public static void Save<A, B>(A obj1, B obj2)
+        {
+            try
+            {
+                DB.Store(obj1);
+                DB.Store(obj2);
+                DB.Commit();
+            }
+            catch
+            {
+                DB.Rollback();
+            }
+        }
+        public static void Delete<T>(T obj)
+        {
+            try
+            {
+                DB.Delete(obj);
+                DB.Commit();
+            }
+            catch
+            {
+                DB.Rollback();
+            }
+        }
     }
 }
